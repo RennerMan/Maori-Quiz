44bac6bdfe420bdf77e0fe4116377ea82dbc6810
RennerMan <rennerj@middleton.school.nz>
2023-05-10 10:57:32 +1200

    Commit

--- Quiz_Questions V2.py
+++ Quiz_Questions V2.py
@@ -2,15 +2,15 @@
 
 import random
 # A list of Maori numbers and their corresponding english numbers
-NUMBERS = [["tahi", "one"], ["rua", "two"], ["toru", "three"], ["wha", "four"], ["rima", "five"], ["ono", "six"],
-           ["whitu", "seven"], ["waru", "eight"], ["iwa", "nine"], ["tekau", "ten"]]
+NUMBERS = [["toru", "three"]]
 # Putting the lists in a different order
 random.shuffle(NUMBERS)
 # Selects the english half of one of the lists and asks what the english word of the list is in Maori
 
 for i in NUMBERS:
-    question = input(f"What is {i[1]} in Maori? ")
-    if question == i[0]:
+    question = input(f"What is {i[1]} in Maori?").lower().split()
+    if question == i[0].lower().split():
         print("Correct answer!")
     else:
         print("Incorrect! You'll get it next time!")
+
