f1d117e2bf7bf2e513f33b3efdc32f429e42b3d4
RennerMan <rennerj@middleton.school.nz>
2023-05-11 11:22:22 +1200

    commit

--- Score_tracker V1.py
+++ Score_tracker V1.py
@@ -1,28 +1,24 @@
-# Component 5: Score Tracker V1-Keeps track of the player's score
+# Component 5: Score Tracker V1-Keeps track of the player's score and length
 
 
 import random
+
 # A list of Maori numbers and their corresponding english numbers
 NUMBERS = [["toru", "three"]]
 
 # Putting the lists in a different order
 random.shuffle(NUMBERS)
 # Selects the english half of the list and asks what the english word of the list is in Maori
-
+length = 0
 score = 0
-for i in NUMBERS:
-    question = input(f"What is {i[1]} in Maori? (type x to quit)").lower().split()
-    if question == i[0].lower().split():
-        print("Correct answer!")
-        score += 1
-    else:
-        print("Incorrect! You'll get it next time!")
+while length != 10:
+    for i in NUMBERS:
+        question = input(f"What is {i[1]} in Maori?").lower().split()
+        length += 1
+        if question == i[0].lower().split():
+            print("Correct answer!")
+            score += 1
+        else:
+            print("Incorrect! You'll get it next time!")
 
-
-print()
-print()
-print()
-print()
-print()
-
-print(f"Game Over! You got {score} questions right!")
+print(f"Game Over! You got {score}/10 questions right!")
