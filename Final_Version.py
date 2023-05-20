aab61f54d3066ad5e05002fa9ecd90e48b92964d
RennerMan <rennerj@middleton.school.nz>
2023-05-20 17:07:03 +1200

    Commit

--- Final_Version.py
+++ Final_Version.py
@@ -27,17 +27,23 @@ def statement_formatter(symbol, statement):
 
 
 def name_age():
-    name = input("What is your name?")
     while True:
-        # Keeps trying an age until it receives a valid age
         try:
-            age = int(input(f"Hi {name}, what is your age?"))
-            # If the age is valid, it breaks the loop
-            break
-        # If it isn't, it tells the player it wasn't valid.
+            # Asks player what their name is
+            name = input("What is your name?").lower().strip()
+            # Creates a loop to check the name is valid
+            if name.isalpha():
+                # If the name is valid, it asks the player's age
+                age = int(input(f"Hi {name}, what is your age?"))
+                # If it is, it breaks the loop
+                break
+                # If it isn't, it shows an error message and loops back
+            else:
+                print("<error> Please enter a valid text")
+                # Checks the age is valid
         except ValueError:
             print("<error> Please enter a valid integer")
-    # Checks they are the right age
+            # Checks to see if the age is between 9 & 12
     if 9 <= age <= 12:
         print("You are around the right age for the quiz!")
     else:
@@ -55,15 +61,15 @@ def played_before():
  Welcome to the Maori Quiz!"))
     print()
     reply = ""
-    while reply != "yes" or "no":
+    while reply != "yes" or "y" or "no" or "n":
         reply = input("Have you done this quiz before?\
-    (Please answer yes or no)").lower().split()
-    # Creates a loop to check the reply is valid
-        if reply == "yes" or reply == "y".split():
+    (Please answer yes or no)").lower().strip()
+        # Creates a loop to check the reply is valid
+        if reply == "yes" or reply == "y":
             print("Nice! We'll send you straight into the quiz")
             print(statement_formatter("#", "Quiz Time"))
             return "yes"
-        elif reply == "no" or reply == "n".split():
+        elif reply == "no" or reply == "n":
             print(statement_formatter("?", "Instructions"))
             print()
             print("You will be given 10 random questions\
@@ -76,12 +82,40 @@ def played_before():
             print()
             print(statement_formatter("#", "Quiz Time"))
             return "no"
-    # Checking to make sure user provides a suitable response
+        # Checking to make sure user provides a suitable response
         else:
             print("Please answer either 'yes' or 'no'")
 
 
 response = played_before()
+
+
+def played_before():
+    print(statement_formatter("#", f"{name_age()}, Welcome to the Maori Quiz!"))
+    print()
+    reply = ""
+    while reply != "yes" or reply != "y" or reply != "no" or reply != "n":
+        reply = input("Have you done this quiz before? (Please answer yes or no)")
+        reply = reply.lower().strip()  # Convert the input to lowercase and remove leading/trailing spaces
+        # Creates a loop to check the reply is valid
+        if reply == "yes" or reply == "y":
+            print("Nice! We'll send you straight into the quiz")
+            print(statement_formatter("#", "Quiz Time"))
+            return "yes"
+        elif reply == "no" or reply == "n":
+            print(statement_formatter("?", "Instructions"))
+            print()
+            print("You will be given 10 random questions about Maori Numbers up to 10")
+            print()
+            print("Try your best, we will show you how many questions you got right out of 10")
+            print()
+            print(statement_formatter("!", "Good Luck"))
+            print()
+            print(statement_formatter("#", "Quiz Time"))
+            return "no"
+        # Checking to make sure user provides a suitable response
+        else:
+            print("Please answer either 'yes' or 'no'")
 
 
 # My Quiz Questions V3 Function
@@ -92,13 +126,18 @@ def questions():
     # Defining the length and score variables
     length = 0
     score = 0
+    # Creating a loop to ensure the quiz is 10 questions long
     while length != 10:
         for i in NUMBERS:
+            # Asks the player what the english half of the list is in Maori
             question = input(f"What is {i[1]} in Maori?").lower().split()
             length += 1
+            # If player answer matches up with the maori half of the list
+            # It tells the player that they got the answer right
             if question == i[0].lower().split():
                 print(statement_formatter("!", "Correct Answer!"))
                 score += 1
+            # If it doesn't, tells them it was incorrect
             else:
                 print(f"Incorrect! It was {i[0]}!\
  You'll get it next time!")
@@ -109,5 +148,6 @@ def questions():
 
 
 test = questions()
+# Tells the player how many questions they got correct out of ten
 print(statement_formatter("-", f"Game Over!\
 You got {test}/10 questions right!"))
